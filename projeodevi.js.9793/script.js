80806410ccc63d0477abc12118af5b6a805cab40
bertugaksu <bertugaksu1@gmail.com>
2024-08-22 17:34:34 +0300

    Add files via upload

---
+++ projeodevi.js.9793/script.js
@@ -0,0 +1,8 @@
+let lookingOnPhone = 0;
+
+
+function myFunction() {
+    lookingOnPhone++;
+document.getElementById("Number").innerHTML = lookingOnPhone;
+document.getElementById("Number").style.color = "#e23232";
+}
